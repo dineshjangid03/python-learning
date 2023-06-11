88f7ec51212ef946b1920dfc4e73a4879dab2c22
dineshjangid03 <dinesh.jangid323@gmail.com>
2023-06-11 11:42:35 +0530

    init

--- main.py
+++ main.py
@@ -96,3 +96,38 @@ j = 0
 while(j < 10) :
     print(j) # 0 1 2 3 4 5 6 7 8 9
     j = j + 1
+
+
+# List
+
+marks = [5,10,9,7,8,6,4]
+
+length_of_marks = len(marks)
+
+element_1 = marks[0]
+element_2 = marks[1]
+element_n = marks[length_of_marks-1]
+
+sub_list_1 = marks[3:] # 3rd index to n
+sub_list_2 = marks[:5] # oth index to 5-1 index
+sub_list_3 = marks[2:4] # 2nd index to 4-1 index
+
+# iterate throw list
+
+sum = 0
+for i in marks:
+    sum = sum + i
+
+sum_1 = 0
+for i in range(length_of_marks):
+    sum_1 =sum_1 + marks[i]
+
+
+# Strings
+
+first_name = "Dinesh"
+last_name = "Kumar"
+
+full_name = first_name + last_name
+
+sub_string = full_name[:6] # 0th index to 6-1 index
